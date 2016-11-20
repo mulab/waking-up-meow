968acd484e12088cec7d1fe5d95edc883b8e8157
TennnyZhuang <zty0826@gmail.com>
2016-11-21 01:50:17 +0800

    generate lint task in manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
 from flask_script import Manager
 from app import create_app
 import os
+import sys
 import unittest
+from pylint import epylint
 
 app = create_app(os.getenv('FLASK_CONFIG', 'default'))
 manager = Manager(app)
@@ -11,6 +13,11 @@ manager = Manager(app)
 def test():
     suite = unittest.TestLoader().loadTestsFromName('tests')
     unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+@manager.command
+def lint():
+    epylint.py_run('app', script='pylint')
 
 if __name__ == '__main__':
     manager.run()
